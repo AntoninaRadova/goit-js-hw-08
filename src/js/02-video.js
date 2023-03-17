16134382257f976ffb48850aa88ba5fc031b0e71
AntoninaRadova <110526254+AntoninaRadova@users.noreply.github.com>
2023-03-17 11:29:55 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,24 +7,16 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(onTimeupdate, 1000));
 
 const SECONDS = localStorage.getItem("videoplayer-current-time:");
-console.log(SECONDS);
-player.setCurrentTime(SECONDS).then(function(SECONDS) {
-    // seconds = the actual time that the player seeked to
-    console.log(SECONDS);
-}).catch(function(error) {
+player.setCurrentTime(SECONDS).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
             console.log(error.message);
-            // the time was less than 0 or greater than the video’s duration
             break;
-
         default:
             console.log(error.name);
-            // some other error occurred
             break;
-    }
+        }
 });
-
 
 function onTimeupdate(e) {
         localStorage.setItem("videoplayer-current-time:", e.seconds);
